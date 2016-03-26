9faaa6ae681d5ab7618ff13a5b258d1e05c00068
Mahtab <roadtocode822@gmail.com>
2016-03-25 21:07:06 -0400

    Added Version 3 of catchat app

---
+++ app/routes/index.js
@@ -0,0 +1,25 @@
+'use strict';
+const h = require('../helpers');
+
+module.exports = () => {
+	let routes = {
+		'get': {
+			'/': (req, res, next) => {
+				res.render('login');
+			},
+			'/rooms': (req, res, next) => {
+				res.render('rooms');
+			},
+			'/chat': (req, res, next) => {
+				res.render('chatroom');
+			}
+		},
+		'post': {
+
+		}
+	}
+
+	
+
+	return h.routes(routes);
+}
